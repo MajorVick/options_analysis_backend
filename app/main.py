643fc3097605233224fa97b991087199ba9d93c7
gaurav23v <gaurav2301v@gmail.com>
2024-11-01 23:12:37 +0530

    added Procfile

--- app/main.py
+++ app/main.py
@@ -14,8 +14,3 @@ app = FastAPI(
 
 # Include routers
 app.include_router(option_chain.router, prefix="/api/v1")
-
-# You can add middleware or exception handlers here if needed
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host=settings.api_host, port=settings.api_port, reload=True)
